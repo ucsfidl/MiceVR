55fdf223824c6ece80e218f0b2bd978b7d9ea095
ucsfidl <ucsfidl@gmail.com>
2017-03-29 12:26:19 -0700

    Keep camera from rotating on form screen
    So you can fill out form without disrupting the mouse's starting view
    angle.

--- Assets/Scripts/Game/DebugControl.cs
+++ Assets/Scripts/Game/DebugControl.cs
@@ -12,26 +12,29 @@ public class DebugControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.A))
-            //this.transform.Translate(Vector3.left * 4f);
-            this.control.Move(Vector3.left*2f);
+        if(this.control.enabled)
+        {
+            if (Input.GetKey(KeyCode.A))
+                //this.transform.Translate(Vector3.left * 4f);
+                this.control.Move(Vector3.left*2f);
 
-        if (Input.GetKey(KeyCode.W))
-            //this.transform.Translate(Vector3.forward * 4f);
-            this.control.Move(Vector3.forward * 2f);
+            if (Input.GetKey(KeyCode.W))
+                //this.transform.Translate(Vector3.forward * 4f);
+                this.control.Move(Vector3.forward * 2f);
 
-        if (Input.GetKey(KeyCode.S))
-            //this.transform.Translate(Vector3.back * 4f);
-            this.control.Move(Vector3.back * 2f);
+            if (Input.GetKey(KeyCode.S))
+                //this.transform.Translate(Vector3.back * 4f);
+                this.control.Move(Vector3.back * 2f);
 
-        if (Input.GetKey(KeyCode.D))
-            //this.transform.Translate(Vector3.right * 4f);
-            this.control.Move(Vector3.right * 2f);
+            if (Input.GetKey(KeyCode.D))
+                //this.transform.Translate(Vector3.right * 4f);
+                this.control.Move(Vector3.right * 2f);
 
-        if (Input.GetKey(KeyCode.Q))
-            this.transform.Rotate(Vector3.up, -2f);
+            if (Input.GetKey(KeyCode.Q))
+                this.transform.Rotate(Vector3.up, -2f);
 
-        if (Input.GetKey(KeyCode.E))
-            this.transform.Rotate(Vector3.up, 2f);
-	}
+            if (Input.GetKey(KeyCode.E))
+                this.transform.Rotate(Vector3.up, 2f);
+        }
+    }
 }
