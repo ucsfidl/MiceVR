c0e129c99a75bf69d0b1d2f76bd9144891910a5b
ucsfidl <ucsfidl@gmail.com>
2018-05-13 10:59:16 -0700

    FVR: new levels.

--- matlab/logTrialMarks.m
+++ matlab/logTrialMarks.m
@@ -23,7 +23,8 @@ if (lastEventTime == 0) % This is the first frame, so log it as the trial start
     % Note that this is labeled as frame #0, not frame #1
 % For unclear reasons there is a slowdown in Unity going in and out of pause, so rely on this to track trial boundaries in the video.
 % 3.5 has false positives (1/3 of videos), pausetime 4 is the same, so try higher threshold at pausetime 3?  2.9 had misses and false positives at pausetime 2, so not hopeful...
-% Will write script to fix this data type
+% Will write script to fix this data type.
+% At this setting (3.5), observe range of times as low as 3.76x and as high as 5.65x
 elseif (etime(d.AbsTime, lastEventTime) > 1/fps * 3.5) 
     d.TimeSinceLastEvent = etime(d.AbsTime, lastEventTime);
     if (numel(trialStarts) == numel(trialEnds))
